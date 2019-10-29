209e5adfa29c91b3440df7a31fdb07b0411576ae
GabrielVieiraCode <Gabriel.vieira.trabalho@gmail.com>
2019-10-29 13:03:33 -0300

    Api controller comunidade

--- Models/AgendaContext.cs
+++ Models/AgendaContext.cs
@@ -24,7 +24,7 @@ namespace API_TW.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+
                 optionsBuilder.UseSqlServer("Server=DESKTOP-V5KAA8D\\SQLEXPRESS;Database=Agenda;Trusted_Connection=True;");
             }
         }
